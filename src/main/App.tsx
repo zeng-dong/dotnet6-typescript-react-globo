417c313b4c9ed037dfc4eca5756a2f900e880f34
Dong Zeng <zeng-dong@hotmail.com>
2022-07-22 13:21:54 -0500

    Header component

---
+++ src/main/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+	return (
+		<div className="container">
+			<Header subtitle="Houses all over the world" />
+		</div>
+	);
+}
+
+export default App;

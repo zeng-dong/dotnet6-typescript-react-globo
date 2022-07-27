7dd0358a6bc9e4e0263e1cc808d1e0d4715e83a3
Dong Zeng <zeng-dong@hotmail.com>
2022-07-26 21:12:01 -0500

    custom hook

---
+++ src/hooks/HouseHooks.ts
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import config from "../config";
+import { House } from "../types/house";
+
+const useFetchHouses = (): House[] => {
+	const [houses, setHouses] = useState<House[]>([]);
+
+	const fetchHouses = async () => {
+		const response = await fetch(`${config.baseApiUrl}/houses`);
+		const houses = await response.json();
+		setHouses(houses);
+	};
+
+	fetchHouses();
+
+	return houses;
+};
+
+export default useFetchHouses;
--- src/house/HouseList.tsx
+++ src/house/HouseList.tsx
@@ -1,8 +1,17 @@
+import config from "../config";
 import { useState } from "react";
 import { House } from "../types/house";
 
 const HouseList = () => {
 	const [houses, setHouses] = useState<House[]>([]);
+
+	const fetchHouses = async () => {
+		const response = await fetch(`${config.baseApiUrl}/houses`);
+		const houses = await response.json();
+		setHouses(houses);
+	};
+
+	fetchHouses();
 
 	return (
 		<div>

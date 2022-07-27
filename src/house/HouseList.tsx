1e77d37290bad5ad717ec55ee0a0eed5c7f83c51
Dong Zeng <zeng-dong@hotmail.com>
2022-07-26 20:49:02 -0500

    houselist

---
+++ src/house/HouseList.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { House } from "../types/house";
+
+const HouseList = () => {
+	const [houses, setHouses] = useState<House[]>([]);
+
+	return (
+		<div>
+			<div className="row mb-2">
+				<h5 className="themeFontColor text-center">
+					Houses currently on the market
+				</h5>
+			</div>
+			<table className="table table-hover">
+				<thead>
+					<tr>
+						<th>Address</th>
+						<th>Country</th>
+						<th>Asking Price</th>
+					</tr>
+				</thead>
+				<tbody>
+					{houses.map((h) => (
+						<tr key={h.id}>
+							<td>{h.address}</td>
+							<td>{h.country}</td>
+							<td>{h.price}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+};
+
+export default HouseList;
--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,3 +1,4 @@
+import HouseList from "../house/HouseList";
 import "./App.css";
 import Header from "./Header";
 
@@ -5,6 +6,8 @@ function App() {
 	return (
 		<div className="container">
 			<Header subtitle="Houses all over the world" />
+
+			<HouseList />
 		</div>
 	);
 }
